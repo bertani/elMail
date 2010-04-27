d74f3646d83fef2e960d75de2212f497f2e395e2
Thomas Bertani <sylar@anche.no>
2010-04-27 07:33:24 +0200

    fixed some licensing issues

--- backend.py
+++ backend.py
@@ -2,13 +2,14 @@
 
  ########################################################################
 #                                                                         #
-#    Copyright (C) 2010 Thomas Bertani <sylar@anche.no>                   #
+#    This code is written by Thomas Bertani <sylar@anche.no>              #
+#    Copyright (C) 2010 Nesit - Via Savelli 128, 35129 Padova, ITALY      #
 #                                                                         #
 #    This file is part of elMail.                                         #
 #                                                                         #
 #    elMail is free software: you can redistribute it and/or modify       #
 #    it under the terms of the GNU General Public License as published by #
-#    the Free Software Foundation, either version 3 of the License, or    #
+#    the Free Software Foundation, either version 2 of the License, or    #
 #    (at your option) any later version.                                  #
 #                                                                         #
 #    elMail is distributed in the hope that it will be useful,            #
